6465d6b26263d03447601b2223033518685c1c95
sjsan <sjsan@DESKTOP-26UUKCE>
2025-08-05 14:48:58 +0530

    inheritence commit

---
+++ JSS_Project/src/com/tnsif/inheritence/MultiLevel.java
@@ -0,0 +1,17 @@
+package com.tnsif.inheritence;
+
+public class MultiLevel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ City ci = new City();
+ ci.setCityName("Bengaluru");
+ ci.setArea("kengeri");
+ ci.setStateName("karnataka");
+ ci.setLanguage("Kannada");
+ ci.setCountryName("INDIA");
+ ci.setCapital("New DELHI");
+ System.out.println(ci);
+	}
+
+}
---
+++ JSS_Project/src/com/tnsif/inheritence/State.java
@@ -0,0 +1,27 @@
+package com.tnsif.inheritence;
+
+public class State extends Country {
+  private String StateName;
+  private String Language;
+  
+  
+ 
+  public String getStateName() {
+	return StateName;
+  }
+  public void setStateName(String stateName) {
+	StateName = stateName;
+  }
+  public String getLanguage() {
+	return Language;
+  }
+  public void setLanguage(String language) {
+	Language = language;
+  }
+  @Override
+  public String toString() {
+	return "State [StateName=" + StateName + ", Language=" + Language + ", getCountryName()=" + getCountryName()
+			+ ", getCapital()=" + getCapital() + "]";
+  }
+    
+}
